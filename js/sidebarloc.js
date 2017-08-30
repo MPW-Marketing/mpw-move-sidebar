7e29b4f4b55a7d74828f20e1a35a1558a3fe6d3e
plainsparky <31480903+plainsparky@users.noreply.github.com>
2017-08-30 13:19:50 -0400

    Update sidebarloc.js

--- js/sidebarloc.js
+++ js/sidebarloc.js
@@ -16,12 +16,10 @@ jQuery(document).ready(function(){
     jQuery(window).resize(function() {
         if (jqMoveUpdateSize() < 761 && !is_moved){
         		jQuery('.page-content').prepend(jQuery('#conv-buttons'));
-            jQuery('.page-content p:nth-of-type(2)').prepend(jQuery('#conv-buttons-b'));
-    			is_moved = true;
+               			is_moved = true;
         } else if (jqMoveUpdateSize() > 760 && is_moved){
         		jQuery('aside.right .widget-1').prepend(jQuery('#conv-buttons'));
-            jQuery('.page-content p:nth-of-type(2)').prepend(jQuery('#conv-buttons-b'));
-    			is_moved = false;
+                			is_moved = false;
         }
     });
 });
